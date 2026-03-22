23dbb4ef8feb1e1b28dfb2d7ae61a434fa72dbb5
Daniil Gnidenko <dagnidenko@gmail.com>
2026-03-22 10:55:46 +0300

    Добавлен crud для habits и public habits

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from habits.models import Habit
+from habits.pagination import HabitPagination
+from habits.permissions import IsOwner
+from habits.serializers import HabitSerializer
+
+
+class HabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+    pagination_class = HabitPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(user=self.request.user).select_related(
+            "user", "related_habit"
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class PublicHabitViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = HabitPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True).select_related(
+            "user", "related_habit"
+        )
